7de04b83e94562957c178ffce49bbd10d69825e3
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-14 16:09:27 -0300

    Add required fields JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 import '../../lib/assets/javascripts/confirm';
 import '../../lib/assets/javascripts/masks';
 import '../../lib/assets/javascripts/fill_address';
+import '../../lib/assets/javascripts/required_fields';
 import 'chartkick/chart.js'
 
 window.Swal = Swal;
