bbd1815a7d82810e77cc704cfdf629deab8fbc80
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-20 16:32:09 -0300

    Add config

---
+++ config/initializers/sanitize_email.rb
@@ -0,0 +1,9 @@
+SanitizeEmail::Config.configure do |config|
+  config[:sanitized_to] = 'ayslanmarcelino@gmail.com'
+  config[:sanitized_cc] = 'ayslanmarcelino@gmail.com'
+  config[:sanitized_bcc] = 'ayslanmarcelino@gmail.com'
+  config[:activation_proc] = Proc.new { !Rails.env.test? && ENV['SANITIZE_EMAIL_ENABLED'] == 'true' }
+  config[:use_actual_email_prepended_to_subject] = true
+  config[:use_actual_environment_prepended_to_subject] = true
+  config[:use_actual_email_as_sanitized_user_name] = true
+end
