40eb80a3f0688e482a13ff3faa5ca51063cc98a7
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-05-03 04:04:11 -0300

    Add controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,11 @@
 class DashboardController < ApplicationController
   def index; end
+
+  def pdf
+    render(
+      pdf: 'file',
+      layout: 'application',
+      template: 'dashboard/report'
+    )
+  end
 end
